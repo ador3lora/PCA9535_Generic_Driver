b624527ff3f6629ff9f114e03c58ce891dd99c4d
Sergei Naumov <squarerootswap@gmail.com>
2022-05-18 14:41:21 +0300

    Fix Convert indentation to spaces

--- pca9535_i2c.c
+++ pca9535_i2c.c
@@ -1,7 +1,7 @@
 /**
  * *****************************************************************************
- * @file	pca9535_i2c.c
- * @brief	Hardware abstraction layer
+ * @file    pca9535_i2c.c
+ * @brief   Hardware abstraction layer
  *
  * *****************************************************************************
  */
@@ -16,26 +16,26 @@
 
 /* Get data from I/O port expander */
 int pca9535_i2c_read(unsigned int dev_idx, unsigned int dev_addr, unsigned char reg_addr, unsigned char *dest, unsigned int len) {
-	if (dev_idx >= PCA9535_USED_NUM ||
-		dest == NULL) {
-		return -2;
-	}
-	/*------------------------------------------------------------------------*/
-	/* Specify a custom I2C read function here -------------------------------*/
-	/* The function must return a value (-1) in case of a hardware error -----*/
-	/*------------------------------------------------------------------------*/
-	return 0;
+    if (dev_idx >= PCA9535_USED_NUM ||
+        dest == NULL) {
+        return -2;
+    }
+    /*------------------------------------------------------------------------*/
+    /* Specify a custom I2C read function here -------------------------------*/
+    /* The function must return a value (-1) in case of a hardware error -----*/
+    /*------------------------------------------------------------------------*/
+    return 0;
 }
 
 /* Send data to I/O port expander */
 int pca9535_i2c_write(unsigned int dev_idx, unsigned int dev_addr, unsigned char reg_addr, unsigned char *src, unsigned int len) {
-	if (dev_idx >= PCA9535_USED_NUM ||
-		src == NULL) {
-		return -2;
-	}
-	/*------------------------------------------------------------------------*/
-	/* Specify a custom I2C write function here ------------------------------*/
-	/* The function must return a value (-1) in case of a hardware error -----*/
-	/*------------------------------------------------------------------------*/
-	return 0;
+    if (dev_idx >= PCA9535_USED_NUM ||
+        src == NULL) {
+        return -2;
+    }
+    /*------------------------------------------------------------------------*/
+    /* Specify a custom I2C write function here ------------------------------*/
+    /* The function must return a value (-1) in case of a hardware error -----*/
+    /*------------------------------------------------------------------------*/
+    return 0;
 }
--- pca9535_i2c.h
+++ pca9535_i2c.h
@@ -1,7 +1,7 @@
 /**
  * *****************************************************************************
- * @file	pca9535_i2c.h
- * @brief	Hardware abstraction layer
+ * @file    pca9535_i2c.h
+ * @brief   Hardware abstraction layer
  *
  * *****************************************************************************
  */
@@ -19,41 +19,41 @@
 /* Export functions prototypes -----------------------------------------------*/
 
 /**
- * @brief		Get data from I/O port expander
- * @param[in]	dev_idx		Device index on the board from 0 to
- * 				PCA9535_USED_NUM - 1
- * @param[in]	dev_addr	Target device address
- * @param[in]	reg_addr	Target register address
- * @param[out]	dest		Buffer for storing data received from the device
- * @param[in]	len			Data buffer length in bytes
+ * @brief       Get data from I/O port expander
+ * @param[in]   dev_idx     Device index on the board from 0 to
+ *              PCA9535_USED_NUM - 1
+ * @param[in]   dev_addr    Target device address
+ * @param[in]   reg_addr    Target register address
+ * @param[out]  dest        Buffer for storing data received from the device
+ * @param[in]   len         Data buffer length in bytes
  * @return
- * 				- (-2): Parameter error
- * 				- (-1): Bus error
- * 				- 0: Success
+ *              - (-2): Parameter error
+ *              - (-1): Bus error
+ *              - 0: Success
  */
-int pca9535_i2c_read(	unsigned int dev_idx,
-						unsigned int dev_addr,
-						unsigned char reg_addr,
-						unsigned char *dest,
-						unsigned int len);
+int pca9535_i2c_read(   unsigned int dev_idx,
+                        unsigned int dev_addr,
+                        unsigned char reg_addr,
+                        unsigned char *dest,
+                        unsigned int len);
 
 /**
- * @brief		Send data to I/O port expander
- * @param[in]	dev_idx		Device index on the board from 0 to
- * 				PCA9535_USED_NUM - 1
- * @param[in]	dev_addr	Target device address
- * @param[in]	reg_addr	Target register address
- * @param[in]	src			Buffer containing data to send
- * @param[in]	len			Data buffer length in bytes
+ * @brief       Send data to I/O port expander
+ * @param[in]   dev_idx     Device index on the board from 0 to
+ *              PCA9535_USED_NUM - 1
+ * @param[in]   dev_addr    Target device address
+ * @param[in]   reg_addr    Target register address
+ * @param[in]   src         Buffer containing data to send
+ * @param[in]   len         Data buffer length in bytes
  * @return
- * 				- (-2): Parameter error
- * 				- (-1): HW error
- * 				- 0: Success
+ *              - (-2): Parameter error
+ *              - (-1): HW error
+ *              - 0: Success
  */
-int pca9535_i2c_write(	unsigned int dev_idx,
-						unsigned int dev_addr,
-						unsigned char reg_addr,
-						unsigned char *src,
-						unsigned int len);
+int pca9535_i2c_write(  unsigned int dev_idx,
+                        unsigned int dev_addr,
+                        unsigned char reg_addr,
+                        unsigned char *src,
+                        unsigned int len);
 
-#endif	/* PCA9535_HAL_H__ */
+#endif  /* PCA9535_HAL_H__ */
